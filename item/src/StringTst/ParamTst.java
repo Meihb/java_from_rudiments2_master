0fb0ff62dc0af5e776d9413045fa2d2afe85635d
harbor.M <1253880904@qq.com>
2018-08-23 17:48:07 +0800

    0823

---
+++ item/src/StringTst/ParamTst.java
@@ -0,0 +1,12 @@
+package StringTst;
+
+public class ParamTst {
+    public static void main(String[] args) {
+        String S1 = "aaaa";
+        String S3 = S1;
+        System.out.println(S3);
+        S1 = "gggg";
+        System.out.println(S3);
+
+    }
+}
